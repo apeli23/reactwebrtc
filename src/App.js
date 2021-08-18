ccd4caa1e3d848b5a2f2e76a2ec57191c4eedf02
breezy <apelibrian@gmail.com>
2021-08-18 21:42:43 +0300

    full commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Streamer from './Components/Streamer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Streamer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Streamer.js
@@ -0,0 +1,230 @@
+import styles from "./Streamer.module.css";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import React, { useRef } from "react";
+
+const Streamer = (props) => {
+  const webcamButtonRef = useRef();
+  const webcamVideoRef = useRef();
+  const callButtonRef = useRef();
+  const callInputRef = useRef();
+  const answerButtonRef = useRef();
+  const remoteVideoRef = useRef();
+  const hangupButtonRef = useRef();
+  const videoDownloadRef = useRef();
+
+  const firestore = firebase.firestore();
+  let videoUrl = null;
+
+  let recordedChunks = [];
+
+  const servers = {
+    iceServers: [
+      {
+        urls: [
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+        ],
+      },
+    ],
+    iceCandidatePoolSize: 10,
+  };
+console.log("servers", servers)
+  // Global State
+  const pc = new RTCPeerConnection(servers);
+  console.log("pc", pc)
+  let localStream = null;
+  let remoteStream = null;
+  var options = { mimeType: "video/webm; codecs=vp9" };
+  let mediaRecorder = null;
+
+  const webCamHandler = async () => {
+    console.log("Starting webcam and mic ..... ");
+    localStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+
+    remoteStream = new MediaStream();
+
+    // Push tracks from local stream to peer connection
+    localStream.getTracks().forEach((track) => {
+      pc.addTrack(track, localStream);
+    });
+
+    // Pull tracks from remote stream, add to video stream
+    pc.ontrack = (event) => {
+      event.streams[0].getTracks().forEach((track) => {
+        remoteStream.addTrack(track);
+      });
+    };
+
+    webcamVideoRef.current.srcObject = localStream;
+    remoteVideoRef.current.srcObject = remoteStream;
+
+    // recording of local video from stream
+    mediaRecorder = new MediaRecorder(localStream, options);
+    mediaRecorder.ondataavailable = (event) => {
+      console.log("data-available");
+      if (event.data.size > 0) {
+        recordedChunks.push(event.data);
+        console.log(recordedChunks);
+      }
+    };
+    mediaRecorder.start();
+  };
+
+  const callHandler = async () => {
+    console.log("Starting callid generation .... ");
+    // Reference Firestore collections for signaling
+    const callDoc = firestore.collection("calls").doc();
+    const offerCandidates = callDoc.collection("offerCandidates");
+    const answerCandidates = callDoc.collection("answerCandidates");
+
+    callInputRef.current.value = callDoc.id;
+
+    // Get candidates for caller, save to db
+    pc.onicecandidate = (event) => {
+      event.candidate && offerCandidates.add(event.candidate.toJSON());
+    };
+
+    // Create offer
+    const offerDescription = await pc.createOffer();
+    await pc.setLocalDescription(offerDescription);
+
+    const offer = {
+      sdp: offerDescription.sdp,
+      type: offerDescription.type,
+    };
+
+    await callDoc.set({ offer });
+
+    // Listen for remote answer
+    callDoc.onSnapshot((snapshot) => {
+      const data = snapshot.data();
+      if (!pc.currentRemoteDescription && data?.answer) {
+        const answerDescription = new RTCSessionDescription(data.answer);
+        pc.setRemoteDescription(answerDescription);
+      }
+    });
+
+    // When answered, add candidate to peer connection
+    answerCandidates.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          const candidate = new RTCIceCandidate(change.doc.data());
+          pc.addIceCandidate(candidate);
+        }
+      });
+    });
+
+    hangupButtonRef.current.disabled = false;
+  };
+
+  const answerHandler = async () => {
+    console.log("Joining the call ....");
+    const callId = callInputRef.current.value;
+    const callDoc = firestore.collection("calls").doc(callId);
+    const answerCandidates = callDoc.collection("answerCandidates");
+    const offerCandidates = callDoc.collection("offerCandidates");
+
+    pc.onicecandidate = (event) => {
+      event.candidate && answerCandidates.add(event.candidate.toJSON());
+    };
+
+    const callData = (await callDoc.get()).data();
+
+    const offerDescription = callData.offer;
+    await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+
+    const answerDescription = await pc.createAnswer();
+    await pc.setLocalDescription(answerDescription);
+
+    const answer = {
+      type: answerDescription.type,
+      sdp: answerDescription.sdp,
+    };
+
+    await callDoc.update({ answer });
+
+    offerCandidates.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        console.log(change);
+        if (change.type === "added") {
+          let data = change.doc.data();
+          pc.addIceCandidate(new RTCIceCandidate(data));
+        }
+      });
+    });
+  };
+
+  const hangupHandler = () => {
+    console.log("Hanging up the call ...");
+
+    localStream.getTracks().forEach((track) => track.stop());
+    remoteStream.getTracks().forEach((track) => track.stop());
+    mediaRecorder.onstop = (event) => {
+      let blob = new Blob(recordedChunks, {
+        type: "video/webm",
+      });
+          //FIXME: Send data to cloudinary
+      videoDownloadRef.current.href = URL.createObjectURL(blob);
+      videoDownloadRef.current.download =
+        new Date().getTime() + "-locastream.webm";
+    };
+    console.log(videoDownloadRef);
+  };
+
+  return (
+    <div>
+      <h2>1. Start your Webcam</h2>
+      <div className={styles.videos}>
+        <span>
+          <h3>Local Stream</h3>
+          <video
+            className={styles.webcamVideo}
+            ref={webcamVideoRef}
+            autoPlay
+            playsInline
+          ></video>
+        </span>
+        <span>
+          <h3>Remote Stream</h3>
+          <video
+            className={styles.webcamVideo}
+            ref={remoteVideoRef}
+            autoPlay
+            playsInline
+          ></video>
+        </span>
+      </div>
+
+      <button onClick={webCamHandler} ref={webcamButtonRef}>
+        Start webcam
+      </button>
+      <h2>2. Create a new Call</h2>
+      <button onClick={callHandler} ref={callButtonRef}>
+        Create Call (offer)
+      </button>
+
+      <h2>3. Join a Call</h2>
+      <p>Answer the call from a different browser window or device</p>
+
+      <input ref={callInputRef} />
+      <button onClick={answerHandler} ref={answerButtonRef}>
+        Answer
+      </button>
+
+      <h2>4. Hangup</h2>
+
+      <button onClick={hangupHandler} ref={hangupButtonRef}>
+        Hangup
+      </button>
+      <a ref={videoDownloadRef} href={videoUrl}>
+        Download session video
+      </a>
+    </div>
+  );
+};
+
+export default Streamer;
